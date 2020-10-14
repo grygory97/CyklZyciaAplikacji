d972cae89598e29f3b28fef8c6aff9dbb9450068
Grzegorz <grzegorz.szawula@pollub.edu.pl>
2020-10-14 16:35:21 +0200

    Fragmenty błąd implementacji

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -1,31 +1,37 @@
 package com.example.aplikacja;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private TextView textView;
-    private String text = "";
+    boolean isSecondFragment = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager().beginTransaction() //Coś do zmiany
+                .add(R.id.fragment_container, new FirstFragment())
+                .commit();
     }
 
 
-    public void onStartBtnClick(View view) {
-        Intent intent = new Intent(this, SecondActivity.class); //inicjalizacja nowej intencji
-        EditText editText = (EditText) findViewById(R.id.editView);
-        String text = String.valueOf(editText.getText());
-        intent.putExtra(SecondActivity.KEY_TEXT, text); //przesłanie naszej danej do 2 activity pierwszy parametr klucz 2 arg dana
-        startActivity(intent);
+    public void onReplaceBtnClick(View view) {
+        if (isSecondFragment) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new SecondFragment())
+                    .commit();
+        } else {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new FirstFragment())
+                    .commit();
+        }
+        isSecondFragment = !isSecondFragment;
     }
 }
